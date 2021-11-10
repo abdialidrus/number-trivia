cdede6fff5419c02fc004b150fa36833f62bc6fb
abdialidrus <muhammadabdi25@gmail.com>
2021-11-10 18:43:56 +0700

    initial commit

---
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:new_number_trivia/core/error/exceptions.dart';
+import 'package:new_number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:new_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  /// Gets the cached [NumberTriviaModel] which was gotten the last time
+  /// the user had an internet connection.
+  ///
+  /// Throws [CacheException] if no cached data is present.
+  Future<NumberTriviaModel> getLastNumberTrivia();
+
+  Future<void> cacheNumberTrivia(NumberTrivia triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTrivia triviaToCache) {
+    return sharedPreferences.setString(
+      CACHED_NUMBER_TRIVIA,
+      json.encode(triviaToCache.toJson()),
+    );
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
+}
---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,26 @@
+import 'package:new_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  const NumberTriviaModel({
+    required String text,
+    required int number,
+  }) : super(
+          text: text,
+          number: number,
+        );
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+}
